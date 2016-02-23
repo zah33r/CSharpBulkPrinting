34b0fd5f64cb3d0023a015f93991059f797929de
zah33r <zaheeer1@gmail.com>
2016-02-23 07:04:03 +0500

    release 1.0.0.0

---
+++ BulkPrintingJobs/frmMain.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace BulkPrinter
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 16);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(221, 388);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedValueChanged += new System.EventHandler(this.listBox1_SelectedValueChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(227, 407);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "[ Select a Printer ]";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(928, 407);
+            this.splitContainer1.SplitterDistance = 227;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(697, 407);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = " [ Selected Files in Folder ]";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(691, 388);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(928, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.selectFilesToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.openToolStripMenuItem.Text = "Select &Directory";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // selectFilesToolStripMenuItem
+            // 
+            this.selectFilesToolStripMenuItem.Name = "selectFilesToolStripMenuItem";
+            this.selectFilesToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F)));
+            this.selectFilesToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.selectFilesToolStripMenuItem.Text = "Select &Files";
+            this.selectFilesToolStripMenuItem.Click += new System.EventHandler(this.selectFilesToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Select the folder containing files to print";
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPrint.Location = new System.Drawing.Point(850, 2);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 5;
+            this.btnPrint.Text = "&Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = resources.GetString("openFileDialog1.Filter");
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "Select files to print";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 431);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bulk Jobs Printer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.ToolStripMenuItem selectFilesToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ BulkPrintingJobs/frmMain.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BulkPrinter
+{
+    public partial class frmMain : Form
+    {
+        #region Local Variables
+
+        private string imagePath = string.Empty;
+
+        #endregion
+
+        #region ctor
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void SendWordToPrinter(string path)
+        {
+            try
+            {
+                Microsoft.Office.Interop.Word.Application wordInstance = new Microsoft.Office.Interop.Word.Application();
+                FileInfo wordFile = new FileInfo(path);
+                object fileObject = wordFile.FullName;
+                object oMissing = System.Reflection.Missing.Value;
+                Microsoft.Office.Interop.Word.Document doc = wordInstance.Documents.Open(ref fileObject, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+                doc.Activate();
+                doc.PrintOut(oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing);
+                wordInstance.Quit();
+            }
+            catch (Exception exp)
+            {
+                string errorMsg = exp.Message;
+            }
+        }
+
+        private void SendToPrinter(string path)
+        {
+            try
+            {
+                ProcessStartInfo info = new ProcessStartInfo();
+                info.Verb = "print";
+                info.FileName = path;
+                info.CreateNoWindow = true;
+                info.WindowStyle = ProcessWindowStyle.Hidden;
+
+                Process p = new Process();
+                p.StartInfo = info;
+                p.Start();
+
+                if (path.ToLower().Trim().EndsWith(".pdf"))
+                {
+                    p.WaitForInputIdle(3000);
+                    System.Threading.Thread.Sleep(3000);
+
+                    if (p != null)
+                        p.Kill();
+                }
+                else
+                    p.WaitForExit();
+
+                System.Threading.Thread.Sleep(5000);
+            }
+            catch (Exception exp)
+            {
+                string errorMsg = exp.Message;
+            }
+        }
+
+        private void listBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string pname = this.listBox1.SelectedItem.ToString();
+                myPrinters.SetDefaultPrinter(pname);
+            }
+            catch (Exception exp)
+            {
+                string errorMsg = exp.Message;
+            }
+        }
+
+        private void listAllPrinters()
+        {
+            foreach (var item in PrinterSettings.InstalledPrinters)
+            {
+                this.listBox1.Items.Add(item.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                listAllPrinters();
+            }
+            catch (Exception exp)
+            {
+                string errorMsg = exp.Message;
+                MessageBox.Show("An error occured while retreiving the list of printers" + Environment.NewLine + Environment.NewLine + "Error Details: " + exp.Message);
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (DialogResult.OK == folderBrowserDialog1.ShowDialog())
+                {
+                    #region Filters
+
+                    dataGridView1.DataSource = null;
+                    List<string> listOfFiles = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.doc", SearchOption.TopDirectoryOnly).ToList<string>();  //docx as well
+                    listOfFiles.AddRange(Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.pdf", SearchOption.TopDirectoryOnly).ToList<string>());
+                    listOfFiles.AddRange(Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.gif", SearchOption.TopDirectoryOnly).ToList<string>());
+                    listOfFiles.AddRange(Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.jpg", SearchOption.TopDirectoryOnly).ToList<string>());
+                    listOfFiles.AddRange(Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.xlsx", SearchOption.TopDirectoryOnly).ToList<string>());
+                    
+                    #endregion
+
+                    UpdateGrid(listOfFiles);
+                }
+            }
+            catch (Exception exp)
+            {
+                string errorMsg = exp.Message;
+            }
+        }
+
+        private void UpdateGrid(List<string> listOfFiles)
+        {
+            try
+            {
+                DataTable table = new DataTable("Files");
+                table.Columns.Add("FileName", typeof(string));
+                table.Columns[0].Caption = "File Name";
+                listOfFiles.ForEach((file) => { table.Rows.Add(file); });
+                dataGridView1.DataSource = table;
+                dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                dataGridView1.Columns[0].HeaderText = "File Name";
+                dataGridView1.Update();
+            }
+            catch (Exception Exp)
+            {
+                string errorMsg = Exp.Message;
+            }
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.DataSource == null)
+                {
+                    MessageBox.Show("Please select a file or directory to proceed.", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+
+                if (DialogResult.Yes == MessageBox.Show("Are you sure, you want to print all files?", this.Text, MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+                {
+
+                    foreach (DataGridViewRow item in dataGridView1.Rows)
+                    {
+                        string path = item.Cells[0].Value.ToString();
+                        item.Cells[0].Selected = true;
+                        try
+                        {
+                            if (path.ToLower().Trim().EndsWith(".jpg") || path.ToLower().Trim().EndsWith(".jpeg") || path.ToLower().Trim().EndsWith(".gif"))
+                            {
+                                this.imagePath = path;
+                                printDocument1.Print();
+                            }
+                            else if (path.ToLower().Trim().EndsWith(".doc") || path.ToLower().Trim().EndsWith(".docx"))
+                            {
+                                SendWordToPrinter(path);
+                            }
+                            else
+                                SendToPrinter(path);
+                        }
+                        catch (Exception innerExp)
+                        {
+                            string errorMsg = innerExp.Message;
+                        }
+                        //Sleep the process for 5 seconds in case files are still getting skipped
+                        //System.Threading.Thread.Sleep(5000);
+                        item.Cells[0].Selected = false;
+                    }
+
+                    MessageBox.Show("All Jobs were sent to printer successfully.", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception exp)
+            {
+                string errorMsg = exp.Message;
+            }
+        }
+
+        private void printDocument1_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            try
+            {
+                System.Drawing.Image imageObject = System.Drawing.Image.FromFile(imagePath);
+                Point location = new Point(100, 100);
+                e.Graphics.DrawImage(imageObject, location);
+            }
+            catch (Exception exp)
+            {
+                string errorMsg = exp.Message;
+            }
+        }        
+
+        private void selectFilesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (DialogResult.OK == openFileDialog1.ShowDialog())
+                {
+                    UpdateGrid(openFileDialog1.FileNames.ToList<string>());
+                }
+            }
+            catch (Exception exp)
+            {
+                string errorMsg = exp.Message;
+            }
+        }
+
+        #endregion
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+    public static class myPrinters
+    {
+        [DllImport("winspool.drv", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool SetDefaultPrinter(string Name);
+
+    }
+}
